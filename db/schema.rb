02a71fc29a543555b24551e5082166b7a073f140
NZTK <tkm.2348nishizawa@gmail.com>
2019-06-10 09:09:32 +0000

    migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_09_091755) do
+ActiveRecord::Schema.define(version: 2019_06_10_062846) do
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -24,6 +24,35 @@ ActiveRecord::Schema.define(version: 2019_06_09_091755) do
     t.index ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
   end
 
+  create_table "artists", force: :cascade do |t|
+    t.string "artist_name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "contact_answers", force: :cascade do |t|
+    t.integer "contact_id"
+    t.integer "admin_id"
+    t.text "answer_title"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "contacts", force: :cascade do |t|
+    t.integer "enduser_id"
+    t.integer "admin_id"
+    t.string "contact_title"
+    t.text "contact_message"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "disks", force: :cascade do |t|
+    t.integer "product_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "endusers", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -34,6 +63,89 @@ ActiveRecord::Schema.define(version: 2019_06_09_091755) do
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_endusers_on_email", unique: true
     t.index ["reset_password_token"], name: "index_endusers_on_reset_password_token", unique: true
+  end
+
+  create_table "favorites", force: :cascade do |t|
+    t.integer "enduser_id"
+    t.integer "product_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "genres", force: :cascade do |t|
+    t.string "genre_name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "labels", force: :cascade do |t|
+    t.string "label_name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "order_details", force: :cascade do |t|
+    t.integer "order_id"
+    t.integer "prodact_id"
+    t.integer "purchase_price"
+    t.integer "purchase_qty"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "orders", force: :cascade do |t|
+    t.integer "enduser_id"
+    t.string "purchase_amount"
+    t.string "freight"
+    t.float "tax"
+    t.string "ship_name"
+    t.string "ship_postal_code"
+    t.string "ship_address"
+    t.integer "payment"
+    t.integer "order_status"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "product_carts", force: :cascade do |t|
+    t.integer "enduser_id"
+    t.integer "product_id"
+    t.integer "product_count"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "products", force: :cascade do |t|
+    t.integer "label_id"
+    t.string "product_name"
+    t.string "jacket_id"
+    t.integer "product_stock"
+    t.string "product_status"
+    t.integer "price"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "songs", force: :cascade do |t|
+    t.string "song_name"
+    t.integer "artist_id"
+    t.integer "genre_id"
+    t.integer "disk_id"
+    t.integer "number"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "user_addresses", force: :cascade do |t|
+    t.integer "enduser_id"
+    t.string "address"
+    t.string "postal_code"
+    t.string "first_name"
+    t.string "family_name"
+    t.string "first_name_kana"
+    t.string "family_mame_kana"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
