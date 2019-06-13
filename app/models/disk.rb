468d4c946f0db305fcf7bd8b95efafaf6b01e928
Azarashi <g.skill.pso@gmail.com>
2019-06-13 18:53:02 +0900

    products

--- app/models/disk.rb
+++ app/models/disk.rb
@@ -1,4 +1,5 @@
 class Disk < ApplicationRecord
-	has_many :songs, dependent: :destroy
-	belongs_to :products
+	belongs_to :product
+	has_many :songs, inverse_of: :disk
+	accepts_nested_attributes_for :songs, reject_if: :all_blank, allow_destroy: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_12_084408) do
+ActiveRecord::Schema.define(version: 2019_06_13_093212) do
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -52,6 +52,7 @@ ActiveRecord::Schema.define(version: 2019_06_12_084408) do
     t.integer "product_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "disk_name"
   end
 
   create_table "endusers", force: :cascade do |t|
@@ -89,12 +90,6 @@ ActiveRecord::Schema.define(version: 2019_06_12_084408) do
   end
 
   create_table "labels", force: :cascade do |t|
-    t.string "label_name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "lebels", force: :cascade do |t|
     t.string "label_name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
