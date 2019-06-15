b2a6bb4993bfe8d96e21c94054210fec5b1cacc9
NZTK <tkm.2348nishizawa@gmail.com>
2019-06-15 09:58:02 +0000

    A

--- app/controllers/endusers_controller.rb
+++ app/controllers/endusers_controller.rb
@@ -1,8 +1,6 @@
 class EndusersController < ApplicationController
   def edit
     @enduser = Enduser.find(params[:id])
-    @enduser_id = current_enduser.id
-    redirect_to enduser_path(@current_enduser.id) if @enduser.id != current_enduser.id
   end
 
   def show
@@ -17,12 +15,13 @@ class EndusersController < ApplicationController
 
   def update
     @enduser = Enduser.find(params[:id])
-    if @enduser.update(enduser_params)
+    if @enduser.update(endusers_params)
       redirect_to enduser_path(@enduser.id)
     else
       render action: "edit"
     end
   end
+
 
   private
 
