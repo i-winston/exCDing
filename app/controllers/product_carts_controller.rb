eedc43cb8b862fd6dd9b0f4556b34a034bee66ef
ryoninomiya <ryoninomiya02@gmail.com>
2019-06-16 19:56:13 +0900

    create add cart

--- app/controllers/product_carts_controller.rb
+++ app/controllers/product_carts_controller.rb
@@ -1,11 +1,28 @@
 class ProductCartsController < ApplicationController
-	def create
-	end
+ 
+  def index
+  	@carts = ProductCart.all
 
-	def destroy
-		product_cart = Porduct.find(params[:product_id])
-		cart = current_enduser.product_carts.new(Porduct_id: Porduct.id)
-		crat.destroy
-		redirect_to puroduct_carts_path
-	end
+
+  end
+
+  def create
+  	@cart = ProductCart.new(product_cart_params)
+  	@cart.enduser_id = current_enduser.id
+    @cart.save
+    redirect_to product_carts_path
+  end
+
+  def destroy
+    product_cart = Product.find(params[:product_id])
+    cart = current_enduser.product_carts.new(Product_id: Product.id)
+    cart.destroy
+    redirect_to product_carts_path
+  end
+
+  private
+
+  def product_cart_params
+    params.require(:product_cart).permit(:enduser_id, :product_id , :product_count)
+  end
 end
