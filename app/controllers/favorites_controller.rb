455244dc49759ca48b4427d157814e31303481a2
NZTK <tkm.2348nishizawa@gmail.com>
2019-06-19 06:38:25 +0000

    A

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -7,16 +7,11 @@ class FavoritesController < ApplicationController
   end
 
   def destroy
-  	if  current_view = @product
+
 	    product = Product.find(params[:product_id])
 	    favorite = current_enduser.favorites.find_by(product_id: product.id)
 	    favorite.destroy
 	    redirect_to product_path(product)
-	else
-		product = Product.find(params[:product_id])
-	    favorite = current_enduser.favorites.find_by(product_id: product.id)
-	    favorite.destroy
-		redirect_to enduser_path(current_enduser.id)
-	end
+
   end
 end
