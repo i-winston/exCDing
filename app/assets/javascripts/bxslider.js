82427b935efe82b9126f29183bd8c8713997c33a
NZTK <tkm.2348nishizawa@gmail.com>
2019-06-22 09:44:48 +0000

    [FIX]SlideShow

--- app/assets/javascripts/bxslider.js
+++ app/assets/javascripts/bxslider.js
@@ -1,8 +1,23 @@
 $(document).ready(function(){
-  $('.bxSlider').bxSlider({
-  		mode "vertical"
+  $('.bxslider').bxSlider({
+    auto: true,           // 自動スライド
+    speed: 1000,          // スライドするスピード
+           // 移動するスライド数
+    pause: 2000,          // 自動スライドの待ち時間
+          // 一度に表示させる最大数
+          // 各スライドの幅
+	randomStart: true,    // 最初に表示するスライドをランダムに設定
+    autoHover: true       // ホバー時に自動スライドを停止
   });
 });
-
-
-
+$(document).ready(function(){
+  $('.bxslider-2').bxSlider({
+   ticker: true,
+    minSlides: 2,
+    maxSlides: 3,
+    slideWidth: 100,
+    slideMargin: 10,
+    tickerHover: true,
+    speed: 30000
+  });
+});
