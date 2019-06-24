4174cd10c38d429c535de3b15f89d62fbbb73c9d
NZTK <tkm.2348nishizawa@gmail.com>
2019-06-24 10:36:26 +0000

    S

--- app/controllers/endusers_controller.rb
+++ app/controllers/endusers_controller.rb
@@ -10,8 +10,9 @@ class EndusersController < ApplicationController
   def show
     @favorites = Favorite.where(enduser_id: @current_enduser.id)
     # @product = Product.find_by(id: favorites.product_id)
-    @history = Order.order("created_at DESC")
+    
     @history = Order.where(enduser_id: @current_enduser.id )
+    @history = Order.order("created_at DESC")
 
   end
 
